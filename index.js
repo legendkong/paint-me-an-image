ff53c4cb70acab38ebae02656fe8c5e60bbba98e
Jonathan Kong <jonathankong1996@gmail.com>
2022-12-09 17:30:13 +0800

    feat: middleware for prompts and image size

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 5001
 
 const app = express()
 
+// Enable body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/openai', require('./routes/openaiRoute'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
