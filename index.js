082be6bab236192082487abc5ca90cfb10d5f1b4
Jonathan Kong <jonathankong1996@gmail.com>
2022-12-09 16:56:06 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5001
+
+const app = express()
+
+app.use('/openai', require('./routes/openaiRoute'))
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
